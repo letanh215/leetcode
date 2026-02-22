b03f2919fd01f2f8dbf21a89a563a244eee3afa6
letanh215 <letanh215@gmail.com>
2026-02-22 13:19:08 +0800

    Bit Mani

---
+++ BitManipulation/test.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+
+#include "SingleNumber.cpp"
+
+using namespace std;
+
+int main() {
+  Solution sol;
+
+  cout << (3 ^ 2) << endl;
+  cout << (1 ^ 3) << endl;
+  
+  return 0;
+}
